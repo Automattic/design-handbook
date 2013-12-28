406dac741e8a5f395d72759cf70163495608bdf8
Michael Arestad <michaelarestad@Michaels-MacBook-Pro.local>
2013-12-27 20:21:52 -0700

    Started button styles.

--- index.php
+++ index.php
@@ -4,8 +4,38 @@
 <div class="content-wrapper">
 	<div class="main">
 		<div class="primary">
+			<h1>Component</h1>
 			<div class="sub">
-				<h1>Howdy!</h1>
+				<h2>Buttons</h2>
+				<h3>&lt;button&gt;</h3>
+				<button>I'm a BUTTON!!!</button>
+				<button>Me too!</button>
+				<button disabled>Disabled :(</button>
+				<button disabled>*sniff*</button>
+				<h3>.button-primary</h3>
+				<a class="button-primary">I'm a BUTTON!!!</a>
+				<a class="button-primary">Me too!</a>
+				<a class="button-primary" disabled>Disabled :(</a>
+				<a class="button-primary" disabled>*sniff*</a>
+				<h3>.button-secondary</h3>
+				<a class="button-secondary">I'm a BUTTON!!!</a>
+				<a class="button-secondary">Me too!</a>
+				<a class="button-secondary" disabled>Disabled :(</a>
+				<a class="button-secondary" disabled>*sniff*</a>
+			</div>
+			<div class="sub">
+				<h2>Form fields</h2>
+				<button>I'm a BUTTON!!!</button>
+				<button>Me too!</button>
+				<button disabled>Disabled :(</button>
+				<button disabled>*sniff*</button>
+			</div>
+			<div class="sub">
+				<h2>Icons</h2>
+				<button>I'm a BUTTON!!!</button>
+				<button>Me too!</button>
+				<button disabled>Disabled :(</button>
+				<button disabled>*sniff*</button>
 			</div>
 		</div>
 	</div>
