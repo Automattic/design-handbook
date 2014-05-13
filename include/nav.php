9d9967ebcd96673b1aca0a9862adf550a40500ef
Shaun Andrews <shaun@automattic.com>
2014-05-13 15:05:48 -0400

    Removing the unfinished/unused sections from the nav

--- include/nav.php
+++ include/nav.php
@@ -7,29 +7,8 @@
 			<h3 class="wpcom-logo"><span class="menu-toggle">Menu</span></h3>
 			<ul>
 				<!-- Let's figure out some content sections! -->
-				<li <?php is_current( $page, 'Typography' ); ?> ><a href="#">Typography</a></li>
-				<li <?php is_current( $page, 'Colors' ); ?> ><a href="colors.php">Colors</a>
-					<ul>
-						<li <?php is_current( $page, 'Colors-Alt' ); ?> ><a href="colors-b.php">Colors-Alt</a></li>
-					</ul>
-				</li>
+				<li <?php is_current( $page, 'Colors' ); ?> ><a href="colors.php">Colors</a></li>
 				<li <?php is_current( $page, 'Components' ); ?> ><a href="index.php">Components</a></li>
-				<li <?php is_current( $page, 'Cheat Sheet' ); ?> ><a href="#">Cheat sheet</a></li>
-				<li><a href="#">1st level</a>
-					<ul>
-						<li><a href="#">2nd level</a>
-							<ul>
-								<li><a href="#">3rd level</a></li>
-								<li><a href="#">Link</a></li>
-								<li><a href="#">Link</a></li>
-								<li><a href="#">Link</a></li>
-							</ul>
-						</li>
-						<li><a href="#">Link</a></li>
-						<li><a href="#">Link</a></li>
-						<li><a href="#">Link</a></li>
-					</ul>
-				</li>
 			</ul>
 		</nav>
 	</div>
