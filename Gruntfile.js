0ea9d8fb43409ab538ebc5d6f55edd1a832ee377
MichaelArestad <marestad@gmail.com>
2014-05-22 00:16:06 -0600

    Updated Gruntfile slightly

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,7 +24,7 @@ module.exports = function(grunt) {
                     expand: true,
                     cwd: 'images/',
                     src: ['**/*.{png,jpg,gif}'],
-                    dest: 'images/build/'
+                    dest: 'images/'
                 }]
             }
         },
